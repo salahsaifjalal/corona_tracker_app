f2a8a875a1d8c017edcb978501ec02047eedeb96
Salahuddin Muhammad <45813964+salahsaifjalal@users.noreply.github.com>
2020-07-07 17:12:35 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
+import styles from './App.module.css';
+
+// Remember! It is not Natural disaster.
+
+import {Cards,Charts,Countries } from './All_Components';
+
 class App extends React.Component{
     render() {
         return(
-            <div>
-                <h1>
-                    Salah App
-                </h1>
+            <div className={styles.container}>
+            
+                    <Cards/>
+                    <Charts/>
+                    <Countries/>
             </div>
         )
 
