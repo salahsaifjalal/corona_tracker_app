a8276bdec98f918c400b5293815e3532ca70deb7
Salahuddin Muhammad <45813964+salahsaifjalal@users.noreply.github.com>
2020-07-09 14:51:32 +0500

    Update Cards.jsx

--- src/All_Components/All_Cards/Cards.jsx
+++ src/All_Components/All_Cards/Cards.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid} from '@material-ui/core';
+import styles from './Cards.module.css';
 
-const Cards =() => {
+
+
+const Cards =({data: {confirmed, recovered, deaths, lastUpdate}}) => {
+   console.log(confirmed);
+    if(!confirmed){
+        return "Data is being loaded, please wait....";
+    }
+    
+
     return (
-        <h1> This is cards.jsx file </h1>
+        <div className={styles.container}>
+            <Grid container spacing = {3} justify="center">
+                <Grid item component = {Card}>
+                    <CardContent>
+                        <Typography color="body1" gutterBottom>Infected People</Typography>
+                        <Typography variant="overline">{confirmed.value} </Typography>
+                        <Typography color="overline">REAL DATA </Typography>
+                        <Typography variant="body1">Number of active cases of COVID-19</Typography>
+                        </CardContent>
 
-    )
+                </Grid>
+                <Grid item component = {Card}>
+                    <CardContent>
+                        <Typography color="body1" gutterBottom>Recovered People</Typography>
+                        <Typography variant="overline">{recovered.value} </Typography>
+                        <Typography color="overline">REAL DATA </Typography>
+                        <Typography variant="body1">Number of recovered cases of COVID-19</Typography>
+                        </CardContent>
+
+                </Grid>
+                <Grid item component = {Card}>
+                    <CardContent>
+                        <Typography color="body1" gutterBottom>Deaths</Typography>
+                        <Typography variant="overline">{deaths.value} </Typography>
+                        <Typography color="overline">REAL DATA </Typography>
+                        <Typography variant="body1">Number of deaths caused by COVID-19</Typography>
+                        </CardContent>
+
+                </Grid>
+            </Grid>
+        </div>
+
+    );
 
 
 } 
