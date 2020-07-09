c80c8f28ec3039e04e36eee5df528dc935380d31
Salahuddin Muhammad <45813964+salahsaifjalal@users.noreply.github.com>
2020-07-09 14:52:08 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
-
 import styles from './App.module.css';
+import {Cards,Charts,Countries } from './All_Components';
+import {fetchData} from './API_Folder';
+
 
 // Remember! It is not Natural disaster.
 
-import {Cards,Charts,Countries } from './All_Components';
+
 
 class App extends React.Component{
+    state = {
+        data:{},
+    }
+
+   async  componentDidMount () {
+        const data = await fetchData();
+        this.setState({data });
+   }
     render() {
+        const { data } = this.state;
         return(
             <div className={styles.container}>
-            
-                    <Cards/>
-                    <Charts/>
-                    <Countries/>
+                     <Cards data = {data}/>
+                     <Charts/>
+                     <Countries/>
             </div>
         )
 
