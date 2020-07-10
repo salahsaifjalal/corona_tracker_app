f0dd59b52f89ee2564973f449fd41bc6d69d6355
Salahuddin Muhammad <45813964+salahsaifjalal@users.noreply.github.com>
2020-07-10 11:56:10 +0500

    Update index.js

--- src/API_Folder/index.js
+++ src/API_Folder/index.js
@@ -3,15 +3,38 @@ import axios from 'axios';
 
 const Corona_Api_url = 'https://covid19.mathdro.id/api';
 
-export const fetchData = async () => {
-    try {
-        const { data: { confirmed, recovered, deaths, lastUpdate}} = await axios.get(Corona_Api_url);
-        
-        return  { confirmed, recovered, deaths, lastUpdate }
-        
-        } catch (error) {
-        
-    }
+export const fetchData = async (country) => {
+  let changeableUrl = Corona_Api_url;
 
+  if (country) {
+    changeableUrl = `${Corona_Api_url}/countries/${country}`;
+  }
 
-}
+  try {
+    const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl);
+
+    return { confirmed, recovered, deaths, lastUpdate };
+  } catch (error) {
+    return error;
+  }
+};
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${Corona_Api_url}/daily`);
+
+    return data.map(({ confirmed, deaths, reportDate: date }) => ({ confirmed: confirmed.total, deaths: deaths.total, date }));
+  } catch (error) {
+    return error;
+  }
+};
+
+export const fetchCountries = async () => {
+  try {
+    const { data: { countries } } = await axios.get(`${Corona_Api_url}/countries`);
+
+    return countries.map((country) => country.name);
+  } catch (error) {
+    return error;
+  }
+};
